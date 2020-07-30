fc7cae4dd6aa46edbe2f758cfbb4951a7df18bdb
gmcln-rct <producersama@gmail.com>
2020-07-30 18:29:40 -0400

    Clean up unused components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import React, { useState} from 'react';
-import Navbar from './components/Navbar';
-import Planets from './components/Planets';
-import People from './components/People';
 import Words from './components/Words';
 import { ReactQueryDevtools } from "react-query-devtools";
 
 function App() {
 
-  const [page, setPage] = useState('words');
   
   return (
     <div className="App">
       <h1>Words App</h1>
-      {/* <Navbar setPage={setPage}/> */}
       <div className="content">
         <Words />
       </div>
