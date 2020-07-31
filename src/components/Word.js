876ea853385edcefe2adab34bdababcdb0fde986
gmcln-rct <producersama@gmail.com>
2020-07-30 22:50:05 -0400

    Work on conditional styling

--- src/components/Word.js
+++ src/components/Word.js
@@ -4,9 +4,10 @@ import React from 'react';
 const Word = ({ word, score, tag }) => {
 
     let percentage = (score/1200).toFixed(2);
+    let alpha = ((score/1200)/100).toFixed(2);
 
     return (
-      <div className="card">
+      <div className="card" style={{ background: `rgba(50,50,50,${alpha})` }}>
         <h3>{word}</h3>
         <p>Relevance = {percentage}%</p>
         {/* <p>{percentage}</p> */}
