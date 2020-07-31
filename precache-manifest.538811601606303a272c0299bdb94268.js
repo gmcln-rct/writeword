60f1f1bffe1660f5f868545e034533682d884e0a
gmcln-rct <producersama@gmail.com>
2020-07-31 18:55:06 -0400

    Updates

---
+++ precache-manifest.538811601606303a272c0299bdb94268.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae64abd8e8bd4e4206cf485eae325d5c",
+    "url": "/.github.io/writeword/index.html"
+  },
+  {
+    "revision": "8c39cd684d7cf73666d7",
+    "url": "/.github.io/writeword/static/css/main.1ee26b15.chunk.css"
+  },
+  {
+    "revision": "f0c25a20afce4dde289c",
+    "url": "/.github.io/writeword/static/js/2.17e1586b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/.github.io/writeword/static/js/2.17e1586b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c39cd684d7cf73666d7",
+    "url": "/.github.io/writeword/static/js/main.dcadddb7.chunk.js"
+  },
+  {
+    "revision": "ba15cdde5c3abd1016ae",
+    "url": "/.github.io/writeword/static/js/runtime-main.40188b38.js"
+  }
+]);
