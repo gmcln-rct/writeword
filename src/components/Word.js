a9840662dc7be3eec82a1ff4bf97568e07adc352
gmcln-rct <producersama@gmail.com>
2020-07-31 11:07:49 -0400

    Work on gradated background

--- src/components/Word.js
+++ src/components/Word.js
@@ -5,9 +5,27 @@ const Word = ({ word, score, tag }) => {
 
     let percentage = (score/1200).toFixed(2);
     let alpha = ((score/1200)/100).toFixed(2);
+    let rgb;
+    let fontcolor = "#fff"
+
+    switch(true) {
+      case (alpha > .80):
+        rgb = "225,225,225";
+        fontcolor = "#000";
+        break;
+      case (alpha > .60):
+        rgb = "125,125,125";
+        break;
+      case (alpha > .40):
+        rgb = "50,50,50";
+        break;
+      default:
+        rgb = "0,0,0";
+    }
+
 
     return (
-      <div className="card" style={{ background: `rgba(50,50,50,${alpha})` }}>
+      <div className="card" style={{ background: `rgba(${rgb},0.8)`, color: `${fontcolor}` }}>
         <h3>{word}</h3>
         <p>Relevance = {percentage}%</p>
         {/* <p>{percentage}</p> */}
