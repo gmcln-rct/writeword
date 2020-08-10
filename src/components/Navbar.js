d9cd1b77a5605432496038ec2f8afb04c8e7b427
gmcln-rct <producersama@gmail.com>
2020-08-10 11:32:33 -0400

    Work on share styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,46 @@
 import React, {useContext} from 'react';
 
+import {ReactComponent as Twitter} from '../images/twitter.svg';
 
 const Navbar = () => {
 
-    return ( 
-        <nav role="navigation">
-            <span>The Write Word</span> 
-            <span id="social-share">
-                   <a href="https://www.facebook.com/sharer.php?u=http%3A%2F%2Ftrataka.io%2F" target="_blank" rel="noopener noreferrer">
-                    <img src="../images/fb-icon.svg" alt="Share on Facebook" id="share-icon" aria-label="Share on Facebook Icon" /> </a>
+    return (
+      <nav role="navigation">
+        <span>The Write Word</span>
+        <span id="social-share">
+          <a
+            href="https://www.facebook.com/sharer.php?u=https%3A%2F%2Fwriteword.site%2F"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src="../images/fb-icon.svg"
+              alt="Share on Facebook"
+              id="share-icon"
+              aria-label="Share on Facebook Icon"
+            />{" "}
+          </a>
 
-                <a href="https://twitter.com/intent/tweet?url=http%3A%2F%2Ftrataka.io%2F" target="_blank" rel="noopener noreferrer">
-                    <img src="../images/twitter-icon.svg" alt="Share on Twitter" id="share-icon" aria-label="Share on Twitter Icon" /> </a>
-
-            </span>
-    
-        </nav>
-     );
+          <a
+            href="https://twitter.com/intent/tweet?url=https%3A%2F%2Fwriteword.site%2F"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Twitter
+              alt="Share on Twitter"
+              id="share-icon"
+              aria-label="Share on Twitter Icon"
+            />
+            {/* <img
+              src={twitter}
+              alt="Share on Twitter"
+              id="share-icon"
+              aria-label="Share on Twitter Icon"
+            /> */}
+          </a>
+        </span>
+      </nav>
+    );
 }
  
 export default Navbar;
