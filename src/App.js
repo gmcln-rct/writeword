ef5d4d8f6a9795349abf468f5a253b8801278c6c
gmcln-rct <producersama@gmail.com>
2020-08-17 21:02:39 -0400

    Update gh

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import React, { useState} from 'react';
 
-import { Helmet } from "react-helmet";
-
-
 import InputForm from './components/InputForm';
 import Navbar from './components/Navbar';
 import Words from './components/Words';
