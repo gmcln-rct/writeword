6741802d7a0cdc5ed6713210afba98b62bb5693e
gmcln-rct <producersama@gmail.com>
2020-08-25 18:24:46 -0400

    Style details section

--- src/components/InfoBox.js
+++ src/components/InfoBox.js
@@ -6,7 +6,9 @@ const InfoBox = () => {
             <details id="info-box">
                 <summary>What is The Write Word?</summary>
                 <p>The Write Word is a thesaurus tool that helps writers find word inspiration with synonyms, antonyms, rhymes, related words and more.</p>
-                <p>Developed by Glenn McClanan using the Datamuse API.</p>
+                <p>By design, the app casts a wide net, showing you even distantly related words to help you find the right word for your project.</p>
+                <p>Developed by Glenn McClanan using the (awewome) Datamuse API.</p>
+                <p>Write On!</p>
             </details>
         </>
      );
